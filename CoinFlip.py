dc4e0b16c1a2e36c0b5e59e5ad4a3c88518b85c2
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2020-10-05 21:15:43 +0200

    Add files via upload

---
+++ CoinFlip.py
@@ -0,0 +1,46 @@
+from os import system
+system("title " + "CoinFlip")
+
+import random
+
+import sys,time
+
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(3./90)
+
+heads = """
+  _    _                _     
+ | |  | |              | |    
+ | |__| | ___  __ _  __| |___ 
+ |  __  |/ _ \/ _` |/ _` / __|
+ | |  | |  __/ (_| | (_| \__ \\
+ |_|  |_|\___|\__,_|\__,_|___/
+"""
+tails = """
+ _______    _ _     
+ |__   __|  (_) |    
+    | | __ _ _| |___ 
+    | |/ _` | | / __|
+    | | (_| | | \__ \\
+    |_|\__,_|_|_|___/
+ """
+
+ans = True
+
+while ans:
+    close = input("'Close' to exit ").lower()
+    
+    answer = random.randint(1,2)
+
+    if close == "close":
+        sys.exit()
+
+    elif answer == 1:
+        print(heads)
+
+    elif answer == 2:
+        print(tails)
+
